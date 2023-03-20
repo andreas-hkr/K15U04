031b7838c4260ce3a81873725964af53a8a7023a
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 16:35:11 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Läs in spelkort från en fil, sortera dem, och skriv ut dem på skärmen
         // <färg> <valör>
         // <färg> <valör>
 
+        Card[] cards = new Card[52];
+        int count = 0;
 
+        Scanner fileIn = new Scanner(new File("cards.txt"));
+        while (fileIn.hasNextInt()) {
+            Card c = new Card();
+            c.suit = fileIn.nextInt();
+            c.rank = fileIn.nextInt();
+            cards[count] = c;
+            count++;
+        }
+
+        Arrays.sort(cards, 0, count);
+
+        for (int i=0; i < count; i++) {
+            System.out.println(cards[i]);
+        }
     }
 }
 
